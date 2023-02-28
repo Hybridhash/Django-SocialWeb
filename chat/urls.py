7cb9ce222f69ec12e9f3ee73db85ce488eba414b
MAhmadAzhar <mahmadazhar@outlook.com>
2023-02-28 23:37:40 +0300

    feat: :recycle: Imprving chat room code and exlude redundand code

--- chat/urls.py
+++ chat/urls.py
@@ -4,11 +4,7 @@ from . import views
 
 urlpatterns = [
     path("chat/", views.ChatHome.as_view(), name="chat_home"),
-    # path("chat/<str:room_name>/", views.ChatSpace.as_view(), name="chat_space"),
-    #     path("chat/<str:slug>/", views.ChatHome.as_view(), name="chat_space"),
-    #     path("chat/space_create", views.SpaceCreateView.as_view(), name="space_create"),
-    #     path("chat/space_join", views.SpaceJoinView.as_view(), name="space_join"),
     path("chat/room/<str:slug>/", views.ChatRoom.as_view(), name="chat"),
-    path("create/", views.RoomCreate.as_view(), name="room-create"),
-    path("join/", views.paceJoin.as_view(), name="space_join"),
+    path("create/", views.SpaceCreate.as_view(), name="space_create"),
+    path("join/", views.SpaceJoin.as_view(), name="space_join"),
 ]
